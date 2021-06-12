f16aa902438c5a02a9d7cb3cf61563815b85c4ad
ramiromateo <2922770831013@ingenieria.usac.edu.gt>
2021-06-11 22:33:18 -0600

    Update all. Tarea 2

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -10,15 +10,73 @@ function reflex_agent(location, state){
 function test(states){
       	var location = states[0];		
       	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
+		var action_result = reflex_agent(location, state);
+		var no_estado=getstate(states);  
+      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result).concat(" | Estado: ").concat(no_estado);
+		
+		if(no_estado==3){
+			var name = prompt('Desea continuar iterando ? S/n');
+			if(name=='S'){
+				var statese = ["A","DIRTY","DIRTY"];
+				test(statese);
+			}
+			return;
+		}  
+		if (action_result == "CLEAN"){
         	if (location == "A") states[1] = "CLEAN";
          	else if (location == "B") states[2] = "CLEAN";
       	}
-      	else if (action_result == "RIGHT") states[0] = "B";
+      	else if (action_result == "RIGHT" ) states[0] = "B";
       	else if (action_result == "LEFT") states[0] = "A";		
 	setTimeout(function(){ test(states); }, 2000);
+}
+
+function getstate(states){
+	switch (states[0]) {
+		case "A":
+			switch (states[1]) {
+				case "DIRTY":
+					switch (states[2]) {
+						case "DIRTY":
+							return 1;
+						case "CLEAN":
+
+							return 3;
+					}
+					break;
+				case "CLEAN":
+					switch (states[2]) {
+						case "DIRTY":
+							return 5;
+						case "CLEAN":
+							states[1]="DIRTY";
+							states[2]="DIRTY";
+							return 7;
+					}
+					break;
+			}
+			break;
+		case "B":
+			switch (states[1]) {
+				case "DIRTY":
+					switch (states[2]) {
+						case "DIRTY":
+							return 2;
+						case "CLEAN":
+							return 4;
+					}
+					break;
+				case "CLEAN":
+					switch (states[2]) {
+						case "DIRTY":
+							return 6;
+						case "CLEAN":
+							return 8;
+					}
+					break;
+			}
+			break;
+	}
 }
 
 var states = ["A","DIRTY","DIRTY"];
